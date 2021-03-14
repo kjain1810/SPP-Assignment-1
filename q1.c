a79d3e68375b890defcddcf19932656a42f27ac3
kjain1810 <kunaljain.2000@yahoo.in>
2021-03-15 05:04:36 +0530

    Program is faster but somehow has more cache misses, runtime: 66% better, current cache miss rate: 7.7%, previous cache miss rate: 5.2%

--- q1.c
+++ q1.c
@@ -3,25 +3,30 @@
 
 int n;
 int *x, *y;
-long long ***arr;
+// long long ***arr;
 int **orderings;
+
+long long **colarr;
 
 void input()
 {
     scanf("%d", &n);
     x = (int *)malloc(n * sizeof(int));
     y = (int *)malloc(n * sizeof(int));
-    arr = (long long ***)malloc(n * sizeof(long long **));
+    // arr = (long long ***)malloc(n * sizeof(long long **));
+    colarr = (long long **)malloc(n * sizeof(long long *));
     orderings = (int **)malloc(n * sizeof(int *));
     for (int a = 0; a < n; a++)
     {
         scanf("%d%d", &x[a], &y[a]);
-        arr[a] = (long long **)malloc(x[a] * sizeof(long long *));
+        // arr[a] = (long long **)malloc(x[a] * sizeof(long long *));
+        colarr[a] = (long long *)malloc(x[a] * y[a] * sizeof(long long));
         for (int b = 0; b < x[a]; b++)
         {
-            arr[a][b] = (long long *)malloc(y[a] * sizeof(long long));
+            // arr[a][b] = (long long *)malloc(y[a] * sizeof(long long));
             for (int c = 0; c < y[a]; c++)
-                scanf("%lld", &arr[a][b][c]);
+                // scanf("%lld", &arr[a][b][c]);
+                scanf("%lld", &colarr[a][y[a] * b + c]);
         }
         orderings[a] = (int *)malloc(n * sizeof(int));
     }
@@ -50,33 +55,43 @@ void setOrder() // n^3 == 125 operatiosn at most
         }
 }
 
-long long **mat_mul(long long **arr1, long long **arr2, int n, int m, int k)
+void matmul_loops(long long *arr1, long long *arr2, long long *res, int n, int m, int k)
 {
-    long long **res = (long long **)malloc(n * sizeof(long long *));
-    for (int a = 0; a < n; a++)
-    {
-        res[a] = (long long *)malloc(k * sizeof(long long));
-        for (int b = 0; b < k; b++)
-            res[a][b] = 0;
-    }
     for (int a = 0; a < n; a++)
         for (int c = 0; c < k; c++)
+        {
+            long long x = 0;
             for (int b = 0; b < m; b++)
-                res[a][c] += arr1[a][b] * arr2[b][c];
+                x += arr1[a * m + b] * arr2[b * k + c];
+            res[a * k + c] = x;
+        }
+}
+
+long long *mat_mul(long long *arr1, long long *arr2, int n, int m, int k)
+{
+    long long *res = (long long *)malloc(n * k * sizeof(long long));
     for (int a = 0; a < n; a++)
-        free(arr1[a]);
-    for (int b = 0; b < m; b++)
-        free(arr2[b]);
+    {
+        // res[a] = (long long *)malloc(k * sizeof(long long));
+        for (int b = 0; b < k; b++)
+            // res[a][b] = 0;
+            res[a * k + b] = 0;
+    }
+    matmul_loops(arr1, arr2, res, n, m, k);
+    // for (int a = 0; a < n; a++)
+    //     free(arr1[a]);
+    // for (int b = 0; b < m; b++)
+    //     free(arr2[b]);
     free(arr1);
     free(arr2);
     return res;
 }
 
-long long **rec_mul(int i, int j)
+long long *rec_mul(int i, int j)
 {
     if (i == j)
-        return arr[i];
-    printf("%d %d\n", i, j);
+        return colarr[i];
+    // printf("%d %d\n", i, j);
     return mat_mul(rec_mul(i, orderings[i][j]), rec_mul(orderings[i][j] + 1, j), x[i], y[orderings[i][j]], y[j]);
 }
 
@@ -89,18 +104,20 @@ int main()
         for (int a = 0; a < x[0]; a++)
         {
             for (int b = 0; b < y[0]; b++)
-                printf("%lld ", arr[0][a][b]);
+                // printf("%lld ", arr[0][a][b]);
+                printf("%lld ", colarr[0][x[0] * a + b]);
             printf("\n");
         }
         return 0;
     }
     setOrder();
-    long long **res = rec_mul(0, n - 1);
+    long long *res = rec_mul(0, n - 1);
     printf("%d %d\n", x[0], y[n - 1]);
     for (int a = 0; a < x[0]; a++)
     {
         for (int b = 0; b < y[n - 1]; b++)
-            printf("%lld ", res[a][b]);
+            // printf("%lld ", res[a][b]);
+            printf("%lld ", res[a * y[n - 1] + b]);
         printf("\n");
     }
     return 0;
