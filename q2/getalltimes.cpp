96b1595ba9309ae05a1d1a8cbdd2d12cd0da3c61
kjain1810 <kunaljain.2000@yahoo.in>
2021-03-20 02:45:21 +0530

    Optimization for FW doesnt seem to work

--- q2/getalltimes.cpp
+++ q2/getalltimes.cpp
@@ -8,9 +8,14 @@
 
 int main(int argc, char **argv)
 {
-    std::string x = "./run <cases_q2/t";
-    std::string y = ".txt >cases_q2/0.out";
+    std::string x = "./run <Q2/t";
+    std::string y = " >myout/";
+    std::string z = ".out";
     std::string cases[] = {"6", "19", "29", "31", "43", "49", "75", "77", "78", "91"};
+
+    std::string diffx = "diff ./Q2out/t";
+    std::string diffy = " ./myout/";
+    std::string diffz = ".out | wc -l";
     for (int a = 0; a < 15; a++)
     {
         struct timespec start, stop;
@@ -21,7 +26,7 @@ int main(int argc, char **argv)
             perror("clock gettime");
             return 1;
         }
-        std::string command = x + cases[a] + y;
+        std::string command = x + cases[a] + y + cases[a] + z;
         system(command.c_str());
 
         if (clock_gettime(CLOCK_REALTIME, &stop) == -1)
@@ -33,6 +38,8 @@ int main(int argc, char **argv)
         accum = (stop.tv_sec - start.tv_sec) + (double)(stop.tv_nsec - start.tv_nsec) / (double)BILLION;
         std::cout << command << "\n"
                   << accum << "\n";
+        command = diffx + cases[a] + diffy + cases[a] + diffz;
+        system(command.c_str());
     }
     return 0;
 }
--- q2/q2.c
+++ q2/q2.c
@@ -20,7 +20,7 @@ const int inf = 1e9;
 int V, E;
 int **edges;
 int **dist;
-int stride = 3;
+int stride = 256;
 
 void getinput()
 {
@@ -45,49 +45,62 @@ void getinput()
 
 void FW_tiled()
 {
-    for (int a = 0; a < V; a += stride)
+    for (int k = 0; k < V; k++)
     {
-        // printf("A = %d\n", a);
-        // we first set the values of the (a, a)th BLOCK
-        for (int b = 0; b < stride && a + b < V; b++)
-            for (int d = 0; d < stride && a + d < V; d++)
-            {
-                // printf("\tsetting: %d %d\n", a + b, a + d);
-                for (int e = 0; e < V; e++)
-                    dist[a + b][a + d] = min(dist[a + b][a + d], dist[a + b][e] + dist[e][a + d]);
-            }
-        // now, set the values of the tiles that are in the same row or column as this tile
-        for (int b = 0; b < V; b += stride)
-            if (a != b)
-                for (int c = 0; c < stride && a + c < V; c++)
-                    for (int d = 0; d < stride && b + d < V; d++)
-                    {
-                        // printf("\tsetting: %d %d\n", a + c, b + d);
-                        for (int e = 0; e < V; e++)
-                            dist[a + c][b + d] = min(dist[a + c][b + d], dist[a + c][e] + dist[e][b + d]);
-                    }
-        for (int b = 0; b < V; b += stride)
-            if (a != b)
-                for (int c = 0; c < stride && b + c < V; c++)
-                    for (int d = 0; d < stride && a + d < V; d++)
-                    {
-                        // printf("\tsetting: %d %d\n", b + c, a + d);
-                        for (int e = 0; e < V; e++)
-                            dist[b + c][a + d] = min(dist[b + c][a + d], dist[b + c][e] + dist[e][a + d]);
-                    }
-        // now, the rest of the tiles
-        for (int b = 0; b < V; b += stride)
-            if (b != a)
-                for (int c = 0; c < V; c += stride)
-                    if (c != a)
-                        for (int d = 0; d < stride && b + d < V; d++)
-                            for (int e = 0; e < stride && c + e < V; e++)
-                            {
-                                // printf("\tsetting: %d %d\n", b + d, c + e);
-                                for (int f = 0; f < V; f++)
-                                    dist[b + d][c + e] = min(dist[b + d][c + e], dist[b + d][f] + dist[f][c + e]);
-                            }
+        for (int i1 = 0; i1 < V; i1 += stride)
+        {
+            for (int i2 = 0; i2 < stride && i1 + i2 < V; i2++)
+                for (int j1 = 0; j1 < stride && i1 + j1 < V; j1++)
+                    dist[i1 + i2][i1 + j1] = min(dist[i1 + i2][i1 + j1], dist[i1 + i2][k] + dist[k][i1 + j1]);
+            for (int i2 = 0; i2 < V; i2 += stride)
+                if (i2 != i1)
+                    for (int i3 = 0; i3 < stride && i2 + i3 < V; i3++)
+                        for (int j1 = 0; j1 < stride && i1 + j1 < V; j1++)
+                            dist[i2 + i3][i1 + j1] = min(dist[i2 + i3][i1 + j1], dist[i2 + i3][k] + dist[k][i1 + j1]);
+            for (int i2 = 0; i2 < V; i2 += stride)
+                if (i2 != i1)
+                    for (int i3 = 0; i3 < stride && i1 + i3 < V; i3++)
+                        for (int j1 = 0; j1 < stride && i2 + j1 < V; j1++)
+                            dist[i1 + i3][i2 + j1] = min(dist[i1 + i3][i2 + j1], dist[i1 + i3][k] + dist[k][i2 + j1]);
+            for (int i2 = 0; i2 < V; i2 += stride)
+                if (i2 != i1)
+                    for (int j1 = 0; j1 < V; j1 += stride)
+                        if (j1 != i1)
+                            for (int i3 = 0; i3 < stride && i2 + i3 < V; i3++)
+                                for (int j2 = 0; j2 < stride && j1 + j2 < V; j2++)
+                                    dist[i2 + i3][j1 + j2] = min(dist[i2 + i3][j1 + j2], dist[i2 + i3][k] + dist[k][j1 + j2]);
+        }
     }
+    // for (int a = 0; a < V; a += stride)
+    // {
+    //     // we first set the values of the (a, a)th BLOCK
+    //     for (int e = 0; e < V; e++)
+    //         for (int b = 0; b < stride && a + b < V; b++)
+    //             for (int d = 0; d < stride && a + d < V; d++)
+    //                 dist[a + b][a + d] = min(dist[a + b][a + d], dist[a + b][e] + dist[e][a + d]);
+    //     // now, set the values of the tiles that are in the same row or column as this tile
+    //     for (int e = 0; e < V; e++)
+    //         for (int b = 0; b < V; b += stride)
+    //             if (a != b)
+    //                 for (int c = 0; c < stride && a + c < V; c++)
+    //                     for (int d = 0; d < stride && b + d < V; d++)
+    //                         dist[a + c][b + d] = min(dist[a + c][b + d], dist[a + c][e] + dist[e][b + d]);
+    //     for (int e = 0; e < V; e++)
+    //         for (int b = 0; b < V; b += stride)
+    //             if (a != b)
+    //                 for (int c = 0; c < stride && b + c < V; c++)
+    //                     for (int d = 0; d < stride && a + d < V; d++)
+    //                         dist[b + c][a + d] = min(dist[b + c][a + d], dist[b + c][e] + dist[e][a + d]);
+    //     // now, the rest of the tiles
+    //     for (int f = 0; f < V; f++)
+    //         for (int b = 0; b < V; b += stride)
+    //             if (b != a)
+    //                 for (int c = 0; c < V; c += stride)
+    //                     if (c != a)
+    //                         for (int d = 0; d < stride && b + d < V; d++)
+    //                             for (int e = 0; e < stride && c + e < V; e++)
+    //                                 dist[b + d][c + e] = min(dist[b + d][c + e], dist[b + d][f] + dist[f][c + e]);
+    // }
 }
 
 int main()
